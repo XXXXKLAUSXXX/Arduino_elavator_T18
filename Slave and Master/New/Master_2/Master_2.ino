de50e5ce92098594f05809951906b85647e067ec
XXXXKLAUSXXX <1081582@hr.nl>
2023-12-14 16:41:54 +0100

    Fixing and finsihging

--- Slave and Master/New/Master_2/Master_2.ino
+++ Slave and Master/New/Master_2/Master_2.ino
@@ -1,7 +1,29 @@
+// communication
 #include <Wire.h>
 
 int ChosenFloor = 0;
 int LiveFloor = 0;
+
+// key board
+#include <Keypad.h>
+
+const byte ROWS = 4;
+const byte COLS = 4;
+
+char hexaKeys[ROWS][COLS] = {
+  { '1', '2', '3', 'A' },
+  { '4', '5', '6', 'B' },
+  { '7', '8', '9', 'C' },
+  { '*', '0', '#', 'D' }
+};
+
+byte rowPins[ROWS] = { 40, 22, 24, 26 };
+byte colPins[COLS] = { 28, 30, 32, 34 };
+
+Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
+
+// code
+
 
 void setup() {
   Wire.begin();
@@ -11,30 +33,62 @@ void setup() {
 
 
 void loop() {
-  for(int i = 0; i <= 4; i++){ // read the IR inputs from all the slaves, update the live floor
-  requestFromSlave(i);
+  // communication
+  for (int i = 0; i <= 4; i++) {  // read the IR inputs from all the slaves, update the live floor
+    requestFromSlave(i);
   }
   Serial.println();
-  delay(500);
+  delay(5);
 
-  ChosenFloor = (ChosenFloor + 1) % 4; // cycle 1-4, for testing purposes
-
-for(int i = 0; i <= 4; i++) { //transmit the live floor to all slaves.
+  for (int i = 0; i <= 4; i++) {  //transmit the live floor to all slaves.
     Wire.beginTransmission(i);
     Wire.write(LiveFloor);
-   Wire.write(ChosenFloor);
+    Wire.write(ChosenFloor);
     Wire.endTransmission();
   }
-}
+  // keypad
+  // make this decide chosen floor 
+  int customKey = customKeypad.getKey();
+  switch (customKey) {
+    case 49:
+ChosenFloor = 1;
+      break;
+    case 50:
+ChosenFloor = 2;
+      break;
+    case 51:
+ChosenFloor = 3;
+      break;
+  }
+
+  if (customKey) {
+    Serial.println(customKey);
+  }
+  // moter
+  int DeltaFloor = ChosenFloor - LiveFloor;
+  if ((DeltaFloor) != 0) {
+    // if DeltaFloor is a posetive number the elevator must go up
+    if (DeltaFloor > 0) {
+      //Moter up
+    } else {
+      //Moter down
+    }
+  }
+
+}  //end of loop
 
 
 
- void requestFromSlave(int slave) {
-  Wire.requestFrom(slave, 1); //request 1 byte
+void requestFromSlave(int slave) {
+  Wire.requestFrom(slave, 1);  //request 1 byte
 
-  int temp = Wire.read(); // read 1 byt of the slave and if it is not 0, we've found the live floor.
-  if (temp > 0) {
-    LiveFloor = temp;
+  int tempLF = Wire.read();  // read 1 byt of the slave and if it is not 0, we've found the live floor.
+  int tempCH = Wire.read();  // If a button is pressed on a floor this will tell the master what floor it is
+  if (tempLF > 0) {          // only update if the value is greater then 0
+    LiveFloor = tempLF;
+  }
+  if (tempCH > 0) {  // only update if the value is greater then 0
+    ChosenFloor = tempCH;
   }
   Serial.print(LiveFloor);
   return;
--- Slave and Master/New/Slave_2/Slave_2.ino
+++ Slave and Master/New/Slave_2/Slave_2.ino
@@ -1,35 +1,78 @@
 #include <Wire.h>
 
-#define slave 1
-#define IR 7
-#define LEDGREEN 4
+#define slave 3
+#define IRx 7
+#define BUTTONx 999
+#define LEDGREEN 5
 #define LEDRED 6
 
 int LiveFloor = 0;
 int ChosenFloor = 0;
+bool IR = 0;
+bool BUTTON = 0;
+
+//7SEG display
+// ST_CP pin 12
+const int latchPin = 10;
+// SH_CP pin 11
+const int clockPin = 11;
+// DS pin 14
+const int dataPin = 12;
+// Patterns for characters 0,1,2,3,4,5,6,7,8,9,A,b,C,d,E,F
+int datArray[4] = {
+  //0
+  B10111111,
+  //1
+  B00100100,
+  //2
+  B01011110,
+  //3
+  B01110110
+};
 
 void setup() {
   Wire.begin(slave);
   Serial.begin(9600);
   Wire.onRequest(requestEvent);
   Wire.onReceive(recieveEvent);
+
+    // 7SEG
+    // Setup pins as Outputs
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+
+      digitalWrite(latchPin, LOW);
+
+    // Shift out the bits
+    shiftOut(dataPin, clockPin, MSBFIRST, datArray[1]);
+
+    // ST_CP HIGH change LEDs
+    digitalWrite(latchPin, HIGH);
 }
 
 void loop() {
-  if (digitalRead(IR) == 1) { // now this nested if statement toggles the leds on if the IR is triggered. If this floor is the Chosen Floor the green led will also turn on.
+  IR = !digitalRead(IRx);
+  BUTTON = !digitalRead(BUTTONx);
+
+  if (digitalRead(IR) == 0) {  // now this nested if statement toggles the leds on if the IR is triggered. If this floor is the Chosen Floor the green led will also turn on.
     digitalWrite(LEDRED, HIGH);
-    if (slave == LiveFloor) {
+    Serial.println("LEDFIRE RED ");
+    if (slave == ChosenFloor) {
       digitalWrite(LEDGREEN, HIGH);
-    } else {
-      digitalWrite(LEDGREEN, LOW);
+      Serial.print("GREEN ");
     }
   } else {
     digitalWrite(LEDRED, LOW);
+    digitalWrite(LEDGREEN, LOW);
   }
-}
+  Update7SEG();
+}  // end of loop
 
 void requestEvent() {
-  Wire.write(digitalRead(IR) * slave);  // if the ir is triggered send our slave number over ic2. If it is not we send a 0.
+
+  Wire.write(IR * slave);      // if the ir is triggered send our slave number over ic2. If it is not we send a 0.
+  Wire.write(BUTTON * slave);  // same as the IR but for the button
 }
 
 
@@ -41,4 +84,9 @@ void recieveEvent() {
   Serial.print("ChosenFloor: ");
   Serial.println(ChosenFloor);
   Serial.println();
+}
+
+void Update7SEG() {
+  int i = LiveFloor;
+    shiftOut(dataPin, clockPin, MSBFIRST, datArray[i + 1]);
 }
